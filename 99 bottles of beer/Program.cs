567c4a9878d6755c2cf636922cf91c72ab68d140
Kristen Shea <kstar61@live.com>
2018-08-29 21:12:46 -0500

    99 bottles of beer loop sing along

--- 99 bottles of beer/Program.cs
+++ 99 bottles of beer/Program.cs
@@ -6,7 +6,32 @@ namespace _99_bottles_of_beer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Write a program that prints out the lyrics to the song “99 Bottles of Beer on the Wall”
+
+            Console.WriteLine("Sing along!");
+
+            for (int i = 99; i >= 0; i--)
+            {
+                int beerLeft = i - 1;
+                Console.WriteLine($"{i} bottles of beer on the wall. {i} bottles of beer. You take 1 down, " +
+                    $"pass it around, {beerLeft} bottles of beer on the wall.");
+
+                 
+                if (beerLeft == 0) break;
+
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
         }
     }
 }
